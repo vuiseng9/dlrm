85d842c01b85b29f4baa63d961da90e0844491fd
Chua, Vui Seng <vui.seng.chua@intel.com>
2020-12-06 21:42:03 -0800

    Changes required to reproduce native training and quantization on NNCF e06cc57 2020_12_04

--- bench/dlrm_s_criteo_kaggle.sh
+++ bench/dlrm_s_criteo_kaggle.sh
@@ -17,6 +17,7 @@ fi
 dlrm_pt_bin="python dlrm_s_pytorch.py"
 dlrm_c2_bin="python dlrm_s_caffe2.py"
 
+date
 echo "run pytorch ..."
 # WARNING: the following parameters will be set based on the data set
 # --arch-embedding-size=... (sparse feature sizes)
@@ -31,3 +32,4 @@ CUDA_VISIBLE_DEVICES=0 $dlrm_pt_bin --arch-sparse-feature-size=16 --arch-mlp-bot
 # $dlrm_c2_bin --arch-sparse-feature-size=16 --arch-mlp-bot="13-512-256-64-16" --arch-mlp-top="512-256-1" --data-generation=dataset --data-set=kaggle --raw-data-file=./input/train.txt --processed-data-file=./input/kaggleAdDisplayChallenge_processed.npz --loss-function=bce --round-targets=True --learning-rate=0.1 --mini-batch-size=128 --print-freq=1024 --print-time $dlrm_extra_option 2>&1 | tee run_kaggle_c2.log
 
 echo "done"
+date
